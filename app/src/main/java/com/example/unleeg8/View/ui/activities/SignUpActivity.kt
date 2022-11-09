a5ca13a07a8b1ba110c7e51bb4d2e7313db9582f
luispipe <lflondonor@unal.edu.co>
2022-11-08 20:38:05 -0500

    firebase

--- app/src/main/java/com/example/unleeg8/View/ui/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/unleeg8/View/ui/activities/SignUpActivity.kt
@@ -3,18 +3,42 @@ package com.example.unleeg8.View.ui.activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.unleeg8.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity:AppCompatActivity() {
     lateinit var btnregister:Button
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        firebaseAuth= Firebase.auth
+        val name= findViewById<EditText>(R.id.signupName)
+        val email= findViewById<EditText>(R.id.signupEmail)
+        val birthdate= findViewById<EditText>(R.id.signupBirthdate)
+        val phone= findViewById<EditText>(R.id.signupPhone)
+        val password= findViewById<EditText>(R.id.signupPassword)
+
         btnregister= findViewById(R.id.register)
         btnregister.setOnClickListener {
-            startActivity(Intent(this,login::class.java))
+            createUser(email.text.toString(),password.text.toString())
         }
+    }
+
+    fun createUser(email:String,password:String){
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                Task-> if(Task.isSuccessful){
+                startActivity(Intent(this,login::class.java))
+                }else{
+                    Toast.makeText(baseContext,"try again",Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/unleeg8/View/ui/activities/login.kt
+++ app/src/main/java/com/example/unleeg8/View/ui/activities/login.kt
@@ -3,21 +3,32 @@ package com.example.unleeg8.View.ui.activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.unleeg8.R
 import com.example.unleeg8.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class login: AppCompatActivity() {
     //lateinit var binding: ActivityMainBinding
     lateinit var signupbutton:Button
     lateinit var loginbutton: Button
-
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        // binding= ActivityMainBinding.inflate(layoutInflater)
        // setContentView(binding.root)
         setContentView(R.layout.activity_login)
+
+        firebaseAuth=Firebase.auth
+        val email= findViewById<EditText>(R.id.loginEmail)
+        val password= findViewById<EditText>(R.id.loginPassword)
+
         signupbutton= findViewById(R.id.signup)
         loginbutton= findViewById(R.id.login)
 
@@ -25,7 +36,7 @@ class login: AppCompatActivity() {
             startActivity(Intent(this,SignUpActivity::class.java))
         }
         loginbutton.setOnClickListener {
-            startActivity(Intent(this,HomeActivity::class.java))
+            user_login(email.text.toString(),password.text.toString())
         }
 
         val txtLossPass= findViewById<TextView>(R.id.lossPass)
@@ -33,4 +44,16 @@ class login: AppCompatActivity() {
             startActivity(Intent(this,RecoverActivity::class.java))
         }
     }
+
+    fun user_login(email:String,password:String){
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                Task-> if(Task.isSuccessful){
+                startActivity(Intent(this,HomeActivity::class.java))
+                }else{
+                    Toast.makeText(baseContext,"ERROR",Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
 }
