fc456581e74dfdefeabc91833fea39e41a7297a8
luispipe <lflondonor@unal.edu.co>
2022-11-09 20:16:31 -0500

    firebase restore

--- app/src/main/java/com/example/unleeg8/View/ui/activities/RecoverActivity.kt
+++ app/src/main/java/com/example/unleeg8/View/ui/activities/RecoverActivity.kt
@@ -4,17 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.unleeg8.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RecoverActivity : AppCompatActivity() {
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recover)
 
-        val btnChange= findViewById<Button>(R.id.buttonChange)
+        firebaseAuth= Firebase.auth
+        val email= findViewById<EditText>(R.id.restoreEmail)
+        val btnChange= findViewById<Button>(R.id.restore)
         btnChange.setOnClickListener {
-            startActivity(Intent(this, login::class.java))
+            restorePassword(email.text.toString())
+            // email --> EditText
+            // email.text --> Object
+            // email.text.toString() --> String
         }
 
     }
+    fun restorePassword(email:String){
+        firebaseAuth.sendPasswordResetEmail(email)
+            .addOnCompleteListener(this){
+                Task-> if (Task.isSuccessful){
+                startActivity(Intent(this, login::class.java))
+            }else{
+                Toast.makeText(this, "Restore Fail", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/example/unleeg8/View/ui/fragments/BooksFragment.kt
+++ app/src/main/java/com/example/unleeg8/View/ui/fragments/BooksFragment.kt
@@ -6,16 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.unleeg8.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class BooksFragment : Fragment() {
-
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_books, container, false)
+        firebaseAuth= Firebase.auth
+
+    }
+
+    fun logout(){
+        firebaseAuth.signOut()
     }
 
 }
